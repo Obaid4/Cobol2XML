1aaf6655e9fc2eb7e465d321ae2353917155395c
M. Obaid Aamir <m.obaidaamir@gmail.com>
2021-03-02 15:24:47 +0000

    Test Suite Implemented at the end of the workshop

--- Cobol2XML/src/JunitTests/AllTests.java
+++ Cobol2XML/src/JunitTests/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ wordTest.class })
+@SuiteClasses({ cobolClass.class, NumTest.class, ParserTest.class, SymbolTest.class, TokenTest.class, wordTest.class })
 public class AllTests {
 
 }
